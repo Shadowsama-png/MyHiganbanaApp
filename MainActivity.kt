4d3cc3ec0613721c87d2606dfb68062cce851bb1
Shadowsama-png <imsadshadow@gmail.com>
2025-07-04 21:28:35 +0300

    Ana ekran kodu yüklendi

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myhiganbanaapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
